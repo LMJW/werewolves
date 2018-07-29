677534c4166f2c87390907f45de31c7d34f25deb
LMJW <heysuperming@gmail.com>
2018-07-29 21:23:19 +1000

    rename the startup menu

--- scene/startup.py
+++ scene/startup.py
@@ -18,17 +18,18 @@ class MenuButton(MenuItem):
     def __init__(self, label, callback_func):
         super().__init__(label, callback_func)
 
-    def on_key_press(self, buttons, modifiers):
-        if buttons & pyglet.window.mouse.LEFT:
-            print(self.label +"Mouse is clicked")
+    def on_key_press(self, symbol, modifiers):
+        if symbol & pyglet.window.mouse.LEFT:
+            print(self.label +" is clicked")
+            print(self.callback_func())
 
 class StartUpMenu(Menu):
     is_event_handler = True
     def __init__(self):
         super().__init__()
         m = []
-        m.append(MenuButton("Setting", self.on_new_game()))
-        m.append(MenuButton("Start", self.on_game_setting()))
+        m.append(MenuButton("Setting", self.on_game_setting))
+        m.append(MenuButton("Start", self.on_new_game))
         self.create_menu(m)
 
     def on_new_game(self):
